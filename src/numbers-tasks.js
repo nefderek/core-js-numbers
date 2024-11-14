ab5644f4bc50e3bd286a92ae316577a59982c061
Dmitry <126347176+nefderek@users.noreply.github.com>
2024-11-14 23:53:15 +0700

    fix: Update numbers-tasks.js

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -501,8 +501,9 @@ function roundToSmallestInteger(/* number */) {
  * 5.1  => 6
  * -5.9 => -5
  */
-function roundToLargestInteger(/* number */) {
-  throw new Error('Not implemented');
+function roundToLargestInteger(number) {
+  const rez = Math.ceil(number);
+  return rez;
 }
 
 /**
