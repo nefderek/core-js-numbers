27cb1b03b2920022669457044f467c2032415177
Dmitry <126347176+nefderek@users.noreply.github.com>
2024-11-14 23:49:06 +0700

    fix: Update numbers-tasks.js

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -241,8 +241,9 @@ function toNumber(/* value, def */) {
  *   -2 => -8
  *   0  => 0
  */
-function getCube(/* num */) {
-  throw new Error('Not implemented');
+function getCube(num) {
+  const rez = num ** 3;
+  return rez;
 }
 
 /**
