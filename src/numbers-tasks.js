f2b16aaec503c8390f3d15db89a94682606605b8
Dmitry <126347176+nefderek@users.noreply.github.com>
2024-11-15 00:00:35 +0700

    fix: Update numbers-tasks.js

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -533,7 +533,7 @@ function roundToNearestInteger(number) {
  * 5.4  => 5
  * -5.5 => -5
  */
-function getIntegerPartNumber(/* number */) {
+function getIntegerPartNumber(number) {
   const rez = Math.trunc(number);
   return rez;
 }
