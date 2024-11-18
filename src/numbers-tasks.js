f78b638cc11ef630fe5f13a443912a778003ae03
Dmitry <126347176+nefderek@users.noreply.github.com>
2024-11-19 00:38:29 +0700

    fix: Update numbers-tasks.js

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -276,9 +276,9 @@ function getFibonacciNumber(/* index */) {
  */
 function getSumToN(n) {
   let sum = 0;
-  for (let i = 1; i <= n; i += 1) {
+  for (let i = 1; i <= n; i += 1){
   sum += i;
-}
+  }
   return sum;
 }
 
