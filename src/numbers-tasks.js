22af393a2053269f553dd51a2797623fecd8ce3a
Dmitry <126347176+nefderek@users.noreply.github.com>
2024-11-22 00:07:40 +0700

    fix: Update numbers-tasks.js

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -564,8 +564,9 @@ function getIntegerPartNumber(number) {
  * 1, 2, 3       => 6
  * 0.1, 0.2, 0.3 => 0.6
  */
-function getSumOfNumbers(/* x1, x2, x3 */) {
-  throw new Error('Not implemented');
+function getSumOfNumbers(x1, x2, x3) {
+  const rez = x1 + x2 + x3;
+  return rez;
 }
 
 /**
