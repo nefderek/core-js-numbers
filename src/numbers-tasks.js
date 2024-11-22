419a453a7d838da664cb4a4d7f719e799ae1a5e6
Dmitry <126347176+nefderek@users.noreply.github.com>
2024-11-22 22:00:53 +0700

    fix: Update numbers-tasks.js

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -359,8 +359,9 @@ function numberToStringInBase(number, base) {
  * @example:
  * 12345, 2    => '1.23e+4'
  */
-function toExponential(/* number, fractionDigits */) {
-  throw new Error('Not implemented');
+function toExponential(number, fractionDigits) {
+  const rez = number.toExponential(fractionDigits);
+  return rez;
 }
 
 /**
