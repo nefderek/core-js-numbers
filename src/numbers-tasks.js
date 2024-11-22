a1b46be9de3388747bc509c0d8d874ef2101d021
Dmitry <126347176+nefderek@users.noreply.github.com>
2024-11-22 23:07:54 +0700

    fix: Update numbers-tasks.js

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -375,8 +375,9 @@ function toExponential(number, fractionDigits) {
  * 12345, 2    => '12345.00'
  * 12.345, 1   => '12.3'
  */
-function toFixed(/* number, fractionDigits */) {
-  throw new Error('Not implemented');
+function toFixed(number, fractionDigits) {
+  const rez = number.toFixed(fractionDigits);
+  return rez;
 }
 
 /**
