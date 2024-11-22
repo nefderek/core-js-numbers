9965e6a57c4ca318d43cf5c2745253e400575f34
Dmitry <126347176+nefderek@users.noreply.github.com>
2024-11-22 23:54:03 +0700

    fix: Update numbers-tasks.js

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -392,8 +392,9 @@ function toFixed(number, fractionDigits) {
  * 12345, 7    => '12345.00'
  * 12.345, 4   => '12.35'
  */
-function toPrecision(/* number, precision */) {
-  throw new Error('Not implemented');
+function toPrecision(number, precision) {
+  const rez = number.toPrecision(precision);
+  return rez;
 }
 
 /**
