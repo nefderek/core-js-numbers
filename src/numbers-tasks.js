2464276051666b68a2f7f706e4537712c334c4a0
Dmitry <126347176+nefderek@users.noreply.github.com>
2024-11-24 00:45:45 +0700

    fix: Update numbers-tasks.js

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -616,8 +616,9 @@ function getMaxNumber(firstNumber, secondNumber) {
  * -5, 0 => -5 | -4 | -3 | -2 | -1 | 0
  * -1, 1 => -1 | 0 | 1
  */
-function getRandomInteger(/* min, max */) {
-  throw new Error('Not implemented');
+function getRandomInteger(min, max) {
+  let rez = min + Math.random() * (max + 1 - min);
+  return Math.floor(rez);
 }
 
 /**
