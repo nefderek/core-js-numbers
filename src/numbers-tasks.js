2436424671aac387c5c8e9f4ea70606b95da6bcf
Dmitry <126347176+nefderek@users.noreply.github.com>
2024-11-24 01:13:04 +0700

    fix: Update numbers-tasks.js

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -649,7 +649,7 @@ function getHypotenuse(a, b) {
  * 15 => 8
  */
 function getCountOfOddNumbers(number) {
-    let count = 0;
+  let count = 0;
   for (let i = 0; i <= number; i += 1) {
     if (i % 2 !== 0) {
       count += 1;
